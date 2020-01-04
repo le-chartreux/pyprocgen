df2245e8f4221f8a8ee6195770a472074a0ae69f
UKFG <59126928+UKFG@users.noreply.github.com>
2020-01-04 19:09:40 +0100

    Update testing_probabilities.py

--- testing_probabilities.py
+++ testing_probabilities.py
@@ -6,26 +6,30 @@ import math
 ############# FONCTIONS DE CREATION ##############
 
 def Type_Case():
-    temperature = random.uniform(-20,50)
-    #print(temperature)
-    humidite = random.random()
-    #print(humidite)
-    if humidite >= 0.7 and temperature >= 25 and temperature <= 30 :
-        return "Trop"
-    elif humidite >= 0.5 and temperature >= 10 and temperature <= 25 :
-        return "Feui"
-    elif humidite >= 0.5 and temperature >= 0 and temperature <= 10 :
-        return "Sapn"
-    elif humidite >= 0.3 and temperature >= -10 and temperature <= 5 :
-        return "Taig"
-    elif humidite < 0.3 and temperature <= -5 :
-        return "Toun"
+	temperature = random.uniform(-20,50)
+	#print(temperature)
+	humidite = random.random()
+	#print(humidite)
+	if humidite >= 0.7 and temperature >= 25 and temperature <= 30 :
+		return "Trop"
+	elif humidite >= 0.5 and temperature >= 10 and temperature <= 25 :
+		return "Feui"
+	elif humidite >= 0.5 and temperature >= 0 and temperature <= 10 :
+		return "Sapn"
+	elif humidite >= 0.3 and temperature >= -10 and temperature <= 5 :
+		return "Taig"
+	elif humidite < 0.3 and temperature <= -2.5 :
+		return "Toun"
+
+	elif humidite < 0.3 and temperature > -2.5 and temperature <=20:
+		return "Stpe"
+
 
 
 
 T=[]
 for j in range(100000):
-    T.append(Type_Case())
+	T.append(Type_Case())
 
 print("DsCh : ",T.count("DsCh")/1000)
 print("Toun : ",T.count("Toun")/1000)
