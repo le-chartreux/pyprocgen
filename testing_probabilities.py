27a2b77b0035312817b46fc035874676f5df3175
UKFG <59126928+UKFG@users.noreply.github.com>
2020-01-04 19:34:13 +0100

    Update testing_probabilities.py

--- testing_probabilities.py
+++ testing_probabilities.py
@@ -10,7 +10,7 @@ def Type_Case():
 	#print(temperature)
 	humidite = random.random()
 	#print(humidite)
-	if humidite >= 0.7 and temperature >= 25 and temperature <= 30 :
+	if humidite >= 0.6 and temperature >= 25 and temperature <= 30 :
 		return "Trop"
 	elif humidite >= 0.5 and temperature >= 10 and temperature <= 25 :
 		return "Feui"
@@ -20,9 +20,14 @@ def Type_Case():
 		return "Taig"
 	elif humidite < 0.3 and temperature <= -2.5 :
 		return "Toun"
-
-	elif humidite < 0.3 and temperature > -2.5 and temperature <=20:
+	elif humidite < 0.3 and temperature > -2.5 and temperature <=12.5 :
 		return "Stpe"
+	elif humidite < 0.3 and temperature > 12.5 and temperature <=30 :
+		return "Savn"
+	elif humidite < 0.2 and temperature > 30 :
+		return "DsCh"
+	elif humidite > 0.3 and humidite < 0.45 and temperature < -10:
+		return "RoEG"
 
 
 
@@ -31,19 +36,21 @@ T=[]
 for j in range(100000):
 	T.append(Type_Case())
 
-print("DsCh : ",T.count("DsCh")/1000)
-print("Toun : ",T.count("Toun")/1000)
-print("Voulu : 8.0")
-print("Alpn : ",T.count("Alpn")/1000)
-print("Taig : ",T.count("Taig")/1000)
-print("Voulu : 10.3")
-print("Savn : ",T.count("Savn")/1000)
-print("Voulu : 13.8")
-print("Stpe : ",T.count("Stpe")/1000)
-print("Voulu : 6.9")
-print("Feui : ",T.count("Feui")/1000)
-print("Voulu : 8.7")
 print("Trop : ",T.count("Trop")/1000)
 print("Voulu : 15.5")
+print("Feui : ",T.count("Feui")/1000)
+print("Voulu : 8.7")
 print("Sapn : ",T.count("Sapn")/1000)
 print("Voulu : 2.8")
+print("Taig : ",T.count("Taig")/1000)
+print("Voulu : 10.3")
+print("Toun : ",T.count("Toun")/1000)
+print("Voulu : 8.0")
+print("Stpe : ",T.count("Stpe")/1000)
+print("Voulu : 6.9")
+print("Savn : ",T.count("Savn")/1000)
+print("Voulu : 13.8")
+print("DsCh : ",T.count("DsCh")/1000)
+print("Voulu : 19.1")
+print("RoEG : ",T.count("RoEG")/1000)
+print("Voulu : 7.6")
