fe4b9e71bc66e89d398ab87bd1aef99151f597f1
UKFG <59126928+UKFG@users.noreply.github.com>
2020-01-05 17:31:36 +0100

    Update testing_probabilities.py

--- testing_probabilities.py
+++ testing_probabilities.py
@@ -5,6 +5,23 @@ import math
 
 ############# FONCTIONS DE CREATION ##############
 
+T_Type_Case = []
+T_Type_Case.append(["FTrp",["tempere_tiede","sous-tropical","tropical"],4000,16000])
+T_Type_Case.append(["FPlv",["boreal","tempere_frais"],1000,4000])
+T_Type_Case.append(["FHmd",["boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"],500,8000])
+T_Type_Case.append(["Ftpr",["boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"],250,4000])
+T_Type_Case.append(["FSch",["tempere_tiede","sous-tropical","tropical"],1000,2000])
+T_Type_Case.append(["Stpe",["tempere_frais"],250,500])
+T_Type_Case.append(["MaqS",["boreal"],125,250])
+T_Type_Case.append(["FTSc",["tropical"],500,1000])
+T_Type_Case.append(["MaqD",["tempere_frais","tempere_tiede","sous-tropical","tropical"],125,250])
+T_Type_Case.append(["MaqS",["boreal"],125,250,["sous-humide"]])
+T_Type_Case.append(["DsCh",["boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"],0,125])
+T_Type_Case.append(["TndS",["sous-polaire"],0,125])
+T_Type_Case.append(["Toun",["sous-polaire"],125,1000])
+T_Type_Case.append(["Taig",["polaire"],0,500])
+T_Type_Case.append(["NULL",[""],0,0])
+
 def Between(val,min,max):
 	if val >= min and val <=max :
 		return True
@@ -12,53 +29,50 @@ def Between(val,min,max):
 		return False
 
 
-def Type_Case():
+def Type_Case(T_Type_Case):
 
-	T_Type_Case = []
-	T_Type_Case.append(["FTrp",["tempere_tiede","sous-tropical","tropical"],4000,16000,["surhumide"]])
-	T_Type_Case.append(["FPlv",["boreal","tempere_frais"],1000,4000,["surhumide"]])
-	T_Type_Case.append(["FHmd",["boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"],500,8000,["tres_humide"]])
-	T_Type_Case.append(["Ftpr",["boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"],250,4000,["humide"]])
-	T_Type_Case.append(["FSch",["tempere_tiede","sous-tropical","tropical"],1000,2000,["sous-humide"]])
-	T_Type_Case.append(["Stpe",["tempere_frais"],250,500,["montagne"]])
-	T_Type_Case.append(["MaqS",["boreal"],125,250,["sous-alpin"]])
-	T_Type_Case.append(["FTSc",["tropical"],500,1000,["mi-aride"]])
-	T_Type_Case.append(["MaqD",["tempere_frais","tempere_tiede","sous-tropical","tropical"],125,250,["tres_aride","aride","mi-aride"]])
-	T_Type_Case.append(["MaqS",["boreal"],125,250,["sous-humide"]])
-	T_Type_Case.append(["DsCh",["boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"],0,125,["suraride","tres_aride","aride","mi-aride"]])
-	T_Type_Case.append(["TndS",["sous-polaire"],0,125,["sous-humide"]])
-	T_Type_Case.append(["Toun",["sous-polaire"],125,1000,["humide","tres_humide","surhumide"]])
-	T_Type_Case.append(["Taig",["polaire"],0,500,["humide","tres_humide","surhumide"]])
-	T_Type_Case.append(["NULL",[""],0,0,[""]])
+	Temperature = random.choice(["polaire","sous-polaire","boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"])
+	#print(Temperature)
 
-	Climat = random.choice(["polaire","sous-polaire","boreal","tempere_frais","tempere_tiede","sous-tropical","tropical"])
-	#print(Climat)
+	if Temperature == "polaire":
+		PlAn = random.choice([random.randint(0,125),random.randint(125,250),random.randint(250,500)])
 
-	PlAn = random.choice([125,250,500,1000,2000,4000,8000])
+	elif Temperature == "sous-polaire" :
+		PlAn = random.choice([random.randint(0,125),random.randint(125,250),random.randint(250,500),random.randint(500,1000)])
+
+	elif Temperature == "boreal" :
+		PlAn = random.choice([random.randint(0,125),random.randint(125,250),random.randint(250,500),random.randint(500,1000),random.randint(1000,2000)])
+
+	elif Temperature == "tempere_frais" :
+		PlAn = random.choice([random.randint(0,125),random.randint(125,250),random.randint(250,500),random.randint(500,1000),random.randint(1000,2000),random.randint(2000,4000)])
+
+	elif Temperature == "tempere_tiede" or Temperature == "sous-tropical" :
+		PlAn = random.choice([random.randint(0,125),random.randint(125,250),random.randint(250,500),random.randint(500,1000),random.randint(1000,2000),random.randint(2000,4000),random.randint(4000,8000)])
+
+	elif Temperature == "tropical" :
+		PlAn = random.choice([random.randint(0,125),random.randint(125,250),random.randint(250,500),random.randint(500,1000),random.randint(1000,2000),random.randint(2000,4000),random.randint(4000,8000),random.randint(8000,16000)])
+
 	#print(PlAn)
-
-	PvEv = random.choice(["suraride","tres_aride","aride","aride","mi-aride","sous-humide","humide","tres_humide","surhumide"])
-	#print(PvEv)
-
 
 	i = 0
 
-	while i < len(T_Type_Case) - 1 and not (Climat in T_Type_Case[i][1] and Between(PlAn,T_Type_Case[i][2],T_Type_Case[i][3]) and PvEv in T_Type_Case[i][4]) :
+	while i < len(T_Type_Case) - 1 and not (Temperature in T_Type_Case[i][1] and Between(PlAn,T_Type_Case[i][2],T_Type_Case[i][3])) :
 		i += 1
 	return T_Type_Case[i][0]
 
-#print(Type_Case())
+#print(Type_Case(T_Type_Case))
+
 
 T=[]
 for j in range(100000):
-	T.append(Type_Case())
+	T.append(Type_Case(T_Type_Case))
 
 print("FTrp : ",T.count("FTrp")/1000)
 print("Voulu : 8")
 print("FPlv : ",T.count("FPlv")/1000)
-print("Voulu : ?")
+print("Voulu : 3.5")
 print("FHmd : ",T.count("FHmd")/1000)
-print("Voulu : ?")
+print("Voulu : 10")
 print("Ftpr : ",T.count("Ftpr")/1000)
 print("Voulu : ?")
 print("FSch : ",T.count("FSch")/1000)
@@ -79,7 +93,8 @@ print("Taig : ",T.count("Taig")/1000)
 print("Voulu : 10.3")
 print("Toun : ",T.count("Toun")/1000)
 print("Voulu : 8.0")
-print("NULL : ",T.count("NULL")/1000)
-print("Voulu : 0")
-print("RoEG : ",T.count("RoEG")/1000)
+
+"""print("RoEG : ",T.count("RoEG")/1000)
 print("Voulu : 7.6")
+print("NULL : ",T.count("NULL")/1000)
+print("Voulu : 0")"""
