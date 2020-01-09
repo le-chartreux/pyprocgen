9106a5f927a792568aeb331d8c0c4765395115bb
UKFG <59126928+UKFG@users.noreply.github.com>
2020-01-09 19:42:14 +0100

    updating

--- Procedural_generation_2D.py
+++ Procedural_generation_2D.py
@@ -3,12 +3,11 @@ from packages.Biomes_creation import *
 from packages.Decisional import *
 from packages.Board_functions import *
 
-
-
 ###############################################################
 ##################### CORPS DU PROGRAMME ######################
 ###############################################################
 
+Biomes = Creation_Constantes_Biomes()
 Plateau = Creer_Plateau_Vide()
 Plateau = Placer_1ere_Case(Plateau, Biomes)
 Afficher_Plateau(Plateau)
