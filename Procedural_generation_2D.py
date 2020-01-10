5344cd204af3792574830d60fb30a5dbaa19676a
ROZIER Theo <roziert@hostname.iut2.upmf-grenoble.fr>
2020-01-10 15:24:03 +0100

    Added a python noise module.

--- Procedural_generation_2D.py
+++ Procedural_generation_2D.py
@@ -1,6 +1,7 @@
 from packages.Biomes_creation import *
 from packages.Decisional import *
 from packages.Board_functions import *
+from packages.Perlin_noise import SimplexNoise
 
 ###############################################################
 ##################### CORPS DU PROGRAMME ######################
@@ -10,3 +11,9 @@ Biomes = Creation_Constantes_Biomes()
 Plateau = Creer_Plateau_Vide()
 Plateau = Placer_1ere_Case(Plateau, Biomes)
 Afficher_Plateau(Plateau)
+
+## DEBUG ##
+noise = SimplexNoise()
+value = noise.noise2(68189, 5198481)
+
+print("Noise value : ", value)
