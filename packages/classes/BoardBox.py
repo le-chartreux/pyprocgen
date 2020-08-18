ea93a89da49e15f98df6e6023e655a3555fcb89e
UKFG <ukfg.poubelle@gmail.com>
2020-08-18 20:21:41 +0200

    Better OO style & documentation

--- packages/classes/BoardBox.py
+++ packages/classes/BoardBox.py
@@ -7,14 +7,7 @@
 # CONTENU :
 # - __slots__
 # - __init__()
-# - getters
-# - setters
-# - add_element()
-# - add_line()
-# - get_element()
-# - get_line()
-# - set_element()
-# - __str__()
+# - create_empty_board()
 # ==========================================================
 
 from __future__ import annotations
@@ -22,7 +15,6 @@ from typing import List, Optional
 
 from packages.classes.Board import Board
 from packages.classes.Box import Box
-from packages.classes.Position import Position
 
 
 class BoardBox(Board):
@@ -31,6 +23,11 @@ class BoardBox(Board):
     ###############################################################
     __slots__ = (
     )
+
+    ###############################################################
+    ############################ HINTS ############################
+    ###############################################################
+    TYPE_OF_ELEMENTS: Optional = Optional[Box]
 
     ###############################################################
     ########################### __INIT__ ##########################
@@ -50,157 +47,24 @@ class BoardBox(Board):
         super().__init__(elements)
 
     ###############################################################
-    ########################### GETTERS ###########################
+    ##################### CREATE_EMPTY_BOARD ######################
     ###############################################################
-    def get_elements(self) -> List[List[Box]]:
-        return super(BoardBox, self).get_elements()
-
-    ###############################################################
-    ########################### SETTERS ###########################
-    ###############################################################
-    def set_elements(self, elements: List[List[Box]]) -> None:
-        line = 0
-        column = 0
-        while line < len(elements) and isinstance(elements[line], list) and column == 0:
-            while (
-                    column < len(elements[line])
-                    and (
-                            isinstance(elements[line][column], Box)
-                            or elements[line][column] is None
-                    )
-            ):
-                column += 1
-            if column == len(elements[line]):
-                column = 0
-                line += 1
-
-        if line == len(elements):
-            super(BoardBox, self).set_elements(elements)
-        else:
-            raise Exception(
-                "Error: trying to set _elements for a " + type(self).__name__ +
-                " but at least one element is not a Box."
-            )
-
-    ###############################################################
-    ######################### ADD_ELEMENT #########################
-    ###############################################################
-    def add_element(
-            self,
-            value: Box,
-            line: int
-    ) -> None:
-        if isinstance(value, Box):
-            super(BoardBox, self).add_element(value, line)
-        else:
-            raise Exception(
-                "Error: trying to add an element in a " + type(self).__name__ + "._elements :" +
-                "\nvalue must be a Box, but a " + type(value).__name__ + " is given."
-            )
-
-    ###############################################################
-    ########################### ADD_LINE ##########################
-    ###############################################################
-    def add_line(self, line: Optional[List[Box]] = None) -> None:
-        if line is None:
-            super(BoardBox, self).add_line()
-        elif isinstance(line, list):
-            i = 0
-            while i < len(line) and isinstance(line[i], Box):
-                i += 1
-            if i == len(line):
-                super(BoardBox, self).add_line(line)
-            else:
-                raise Exception(
-                    "Error: impossible to add a line in a " + type(self).__name__ + "._elements :" +
-                    "\nlines must be List[Box], but a List[" + type(line[i]).__name__ + "] is given."
-                )
-        else:
-            raise Exception(
-                "Error: impossible to add a line in a " + type(self).__name__ + "._elements :" +
-                "\nlines must be List[Box], but a " + type(line).__name__ + " is given."
-            )
-
-    ###############################################################
-    ######################### GET_ELEMENT #########################
-    ###############################################################
-    def get_element(
-            self,
-            x: Optional[int] = None,
-            y: Optional[int] = None,
-            position: Optional[Position] = None
-    ) -> Optional[Box]:
+    @classmethod
+    def create_empty_board(cls, width: int, height: int) -> BoardBox:
         # =============================
         # INFORMATIONS :
         # -----------------------------
         # UTILITÉ :
-        # Renvoie l'élément de self._elements en (y, x) ou en (position.y, position.x)
+        # Crée un BoardBox rempli de None, de dimension height x width
         # =============================
-        element = super(BoardBox, self).get_element()
-        if element is None or isinstance(element, Box):
-            return element
+        bullshit_check = super(BoardBox, cls).create_empty_board(width, height)
+        if isinstance(bullshit_check, BoardBox):
+            return bullshit_check
         else:
             raise Exception(
-                "Error: an element of a " + type(self).__name__ + " is a " + type(element).__name__ +
-                ", must be a Box or None"
+                "Error: WTF how could this error happened ???"
             )
 
-    ###############################################################
-    ########################## GET_LINE ###########################
-    ###############################################################
-    def get_line(self, line_number: int) -> List[Optional[Box]]:
-        line = super(BoardBox, self).get_line(line_number)
-        i = 0
-        while i < len(line) and (isinstance(line[i], Box) or line[i] is None):
-            i += 1
-        if i == len(line):
-            return line
-        else:
-            raise Exception(
-                "Error: trying to get a line from a " + type(self).__name__ +
-                "._elements but at least one element is a " + type(line[i]).__name__ + ", must be a Box."
-            )
 
-    ###############################################################
-    ######################### SET_ELEMENT #########################
-    ###############################################################
-    def set_element(
-            self,
-            value: Box = None,
-            x: Optional[int] = None,
-            y: Optional[int] = None,
-            position: Optional[Position] = None
-    ) -> None:
-        # =============================
-        # INFORMATIONS :
-        # -----------------------------
-        # UTILITÉ :
-        # Set l'élément en (y,x) ou en (position.y, position.x) à value
-        # -----------------------------
-        # PRÉCONDITIONS:
-        # - value : instance de Box
-        # - x et y ou position.x et position.y existent et respectent les dimensions de elements.
-        # =============================
-        if isinstance(value, Box):
-            super(BoardBox, self).set_element(value, x, y, position)
-        else:
-            raise Exception(
-                "Error: impossible to set an element in a " + type(self).__name__ + ".elements:" +
-                "\nelement must be a Box, is a " + type(value).__name__ + "."
-            )
-
-    ###############################################################
-    ########################### __STR__ ###########################
-    ###############################################################
-    def __str__(self) -> str:
-
-        str_version = ""
-
-        for line in self.get_elements():
-
-            for element in line:
-                str_version += element.get_biome().get_name()[0:4] + " "
-
-        str_version += "\n"
-
-        return str_version
+a = BoardBox.create_empty_board(0, 0)
+print(a.TYPE_OF_ELEMENTS)
