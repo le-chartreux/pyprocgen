e8692d2fb8d7ff186c5d5c9af8a5c7b64717da8e
UKFG <ukfg.poubelle@gmail.com>
2020-08-21 18:16:08 +0200

    Adding the new classes

--- packages/short_class_import.py
+++ packages/short_class_import.py
@@ -1,9 +1,12 @@
 from packages.classes.Biome import Biome
-from packages.classes.Box import Box
-from packages.classes.Encyclopedia import Encyclopedia
-from packages.classes.Tree import Tree
-from packages.classes.Color import Color
-from packages.classes.Position import Position
 from packages.classes.Board import Board
+from packages.classes.BoardBox import BoardBox
+from packages.classes.BoardColor import BoardColor
+from packages.classes.Box import Box
+from packages.classes.BoxWithTree import BoxWithTree
+from packages.classes.Color import Color
+from packages.classes.Encyclopedia import Encyclopedia
+from packages.classes.Position import Position
+from packages.classes.Tree import Tree
 
 # Sert juste à pouvoir importer en une ligne plusieurs classes
