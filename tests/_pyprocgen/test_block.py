6b669a8e943ec623fa2bf7c4b81a0b734210d09b
le-chartreux <le-chartreux-vert@protonmail.com>
2024-03-25 23:08:55 +0100

    fix: split __eq__ test in 2 tests (one eq one not eq)

--- tests/_pyprocgen/test_block.py
+++ tests/_pyprocgen/test_block.py
@@ -5,5 +5,9 @@ def test___eq__() -> None:
     block1 = Block.ROCK
     block2 = Block.ROCK
     assert block1 is block2
+
+
+def test___eq___not_equal() -> None:
+    block1 = Block.ROCK
     block2 = Block.AIR
     assert block1 is not block2
